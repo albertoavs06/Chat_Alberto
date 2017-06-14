dc133b11e0ac190da6ac4c07e43d77609efb25f6
albertoavs06 <albertoavs06@gmail.com>
2017-06-14 08:22:13 -0300

    first commit

---
+++ ChatSocketAlberto/Servidor/src/com/mballem/app/Servidor.java
@@ -0,0 +1,20 @@
+
+package com.mballem.app;
+
+
+import com.mballem.app.service.ServidorService;
+
+/**
+ *
+ * @author PMRN
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new ServidorService();
+    }
+    
+}
---
+++ RelogioDigital_threads/src/relogiodigital_threads/RelogioDigital_threads.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relogiodigital_threads;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author PMRN
+ */
+public class RelogioDigital_threads extends JFrame implements Runnable{
+
+
+    
+public static void main(String[] args) {
+        
+    }
+
+public relogiodigital_threads{
+    
+}
+
+
+ public void run(){
+     
+ }   
+}
